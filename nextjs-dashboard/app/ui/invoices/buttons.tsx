5187270d17cf460aa28c9002cedccf0925a43abf
unknown <zhen@solution22.com.au>
2025-10-21 13:23:02 +1100

    Chapter 12 mid- 3.Fetch the specific invoice

--- nextjs-dashboard/app/ui/invoices/buttons.tsx
+++ nextjs-dashboard/app/ui/invoices/buttons.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export function CreateInvoice() {
   return (
     <Link
-      href="/dashboard/invoices/create"
+      href={`/dashboard/invoices/${id}/edit`}
       className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
     >
       <span className="hidden md:block">Create Invoice</span>{' '}
--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  experimental: {},
 };
+module.exports = nextConfig;
+
 
 export default nextConfig;
